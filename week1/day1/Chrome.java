d8338ca8336616387d2c19a07f91dbd6cdada0b4
Vanikiruthika <110739591+Vanikiruthika@users.noreply.github.com>
2024-10-15 22:10:46 +0530

    Add files via upload
    Week1-Day1 and Day2 assignments

---
+++ week1/day1/Chrome.java
@@ -0,0 +1,20 @@
+package week1.day1;
+
+public class Chrome {
+	public static void main(String[] args) {
+		float version = 91.0f;
+		String developer = "Google";
+		boolean isBeta = false;
+		int releaseYear = 2008;
+		char shortcutKey = 'C';
+		
+		System.out.println("Chrome Details");
+		System.out.println("*******************");
+		System.out.println("Verion : " + version);
+		System.out.println("Developer : " + developer);
+		System.out.println("Is Beta : " + isBeta);
+		System.out.println("Relase year : " + releaseYear);
+		System.out.println("shortcut key : " + shortcutKey);
+	}
+
+}
---
+++ week1/day2/NumberCheck.java
@@ -0,0 +1,21 @@
+package week1.day2;
+
+public class NumberCheck {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int number = 5;
+		if(number > 0)
+		{
+			System.out.println(number + " is positive number");
+		}
+
+		else if(number < 0){
+			System.out.println(number + " is negative number");
+		}
+		else {
+			System.out.println(number + " is neither positive nor negative number");
+		}
+	}
+
+}
