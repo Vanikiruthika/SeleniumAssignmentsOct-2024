63c2f473efa0e98b5cd20fe94535c8d78f5a14af
Vanikiruthika <110739591+Vanikiruthika@users.noreply.github.com>
2024-10-23 20:11:03 +0530

    day5 assignments

---
+++ day5/homeassignments/CreateLead.java
@@ -0,0 +1,36 @@
+package week2.day5.homeassignments;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+public class CreateLead {
+	public static void main(String[] args) {
+		
+		ChromeDriver driver = new ChromeDriver();
+		driver.manage().window().maximize();
+		driver.get("http://leaftaps.com/opentaps/");
+		driver.findElement(By.id("username")).sendKeys("demosalesmanager");
+		driver.findElement(By.name("PASSWORD")).sendKeys("crmsfa");
+		driver.findElement(By.className("decorativeSubmit")).click();
+		driver.findElement(By.linkText("CRM/SFA")).click();
+		driver.findElement(By.linkText("Leads")).click();
+		driver.findElement(By.linkText("Create Lead")).click();
+		driver.findElement(By.id("createLeadForm_companyName")).sendKeys("Elsevier");
+		driver.findElement(By.id("createLeadForm_firstName")).sendKeys("Vani");
+		driver.findElement(By.id("createLeadForm_lastName")).sendKeys("Dhandapani");
+		driver.findElement(By.id("createLeadForm_generalProfTitle")).sendKeys("Sr Quality test Engineer");
+		driver.findElement(By.name("submitButton")).click();
+		String title = driver.getTitle();
+		System.out.println("Title of the page: " + title);
+		if(title.contains("View Lead"))
+		{
+			System.out.println("Web page title is verified");
+		}
+		else
+		{
+			System.out.println("Web page title is not verified");
+		}
+		
+	}
+
+}
